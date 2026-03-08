9ffe88e69becf112ce228aeef9851f68f86f127c
EAPerez007 <157754604+codisi97@users.noreply.github.com>
2026-03-07 21:21:16 -0600

    Pagina drivers

--- tarea3/src/app/app.routes.ts
+++ tarea3/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Drivers } from './pages/drivers/drivers';
 
-export const routes: Routes = [ { path: 'dashboard', component: Dashboard } ];
+export const routes: Routes = [ { path: 'dashboard', component: Dashboard } , {path: 'drivers', component:Drivers }];
